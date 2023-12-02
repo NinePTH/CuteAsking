a858e4700c3d4cdcfe0c48d8f2637d3a0c3c40a8
Patihan Prasongphol <patihan70@gmail.com>
2023-12-02 22:13:11 +0700

    modifyConstants

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "sooo cute :), see you at night";
+  question.innerHTML = "sooo cute :), see you.";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
   alert("Thank youuuuuuuuuuuuuuuu!");
